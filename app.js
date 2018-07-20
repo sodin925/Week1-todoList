6082057f366a12e82cf9ace05a26ccf3df65e7bd
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-07-20 19:31:31 +0800

    .

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-var express = require('express');
-var app = express();
+var Http = require('http'),
+  Router = require('router'),
+  server,
+  router;
+router = new Router();
 
-app.get('/', function (req, res) {
-  res.send('index.html');
+server = Http.createServer(function (request, response) {
+  router(request, response, function (error) {
+    if (!error) {
+      response.writeHead(404);
+    } else {
+      // Handle errors
+      console.log(error.message, error.stack);
+      response.writeHead(400);
+    }
+    response.end('RESTful API Server is running!');
+  });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+server.listen(3000, function () {
+  console.log('Listening on port 3000');
 });
+
+
+var counter = 0,
+  todoList = {};
+
+function createItem(request, response) {
+  var id = counter += 1;
+  console.log('Create item', id);
+  response.writeHead(201, {
+    'Content-Type': 'text/plain'
+  });
+  response.end('Item ' + id);
+}
+router.post('/todo', createItem);
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 $(function () {
 
-
   $(".heart").on("click", function () {
     if ($(this).prop("name") == "heart-empty") {
       $(this).parent("div").css("background", "#FFFFBB");
@@ -18,8 +17,10 @@ $(function () {
     if (this.checked) {
       $(this).parent("div").css("background", "#808080");
       document.getElementById("delete").disabled = false;
+      var x = this;
       if ($("#delete").on("click", function () {
-          $(this).parent("div").remove();
+          $(x).parent("div").remove();
+          document.getElementById("delete").disabled = true;
         }));
     } else {
       $(this).parent("div").css("background", "#F2F2F2");
@@ -28,3 +29,16 @@ $(function () {
   });
 
 });
+
+function newlist() {
+  var li = document.createElement("li");
+  var nameinput = document.getElementById("nameinput").value;
+  var n = document.createTextNode(nameinput);
+  li.appendChild(n);
+  if(nameinput === "") {
+    alert("you need to write something");
+  } else {
+    document.getElementById("mylist").appendChild(li);
+  }
+  document.getElementById("nameinput").value = "";
+}
