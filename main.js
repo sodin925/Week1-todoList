f12119caf066a8e46b881fa107a7a8e9b3290718
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-07-26 18:33:16 +0800

    checkbox

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 $(function () {
 
   $(".heart").on("click", function () {
@@ -25,19 +26,47 @@ $(function () {
       document.getElementById("delete").disabled = true;
     }
   });
-});
+  $.ajax({
+    url: "http://localhost:3000/todo",
+    method: "get",
+    dataType: "json",
+    data: {},
+  }).done(function (res) {
+    // console.log(res);
+    $("#mylist").empty();
+    res.forEach(function (todo) {
+      console.log(todo);
+      var q = document.createElement("input");
+      q.setAttribute("type", "checkbox");
+      q.setAttribute("class", "checkbox");
+      $("#mylist").append("<li>" + "名稱: " + todo.title + "   " + "到期日: " + todo.time + "   " + "備註: " + todo.memo + "   " + "</li>");
+      var li = document.getElementsByTagName("li");
+      for (var i = 0; i < li.length; i++) {
+        var span = document.createElement("SPAN");
+        li[i].appendChild(span);
+        span.appendChild(q);
+      }
+        console.log(li);
+    });
 
-function newlist() {
-  var ul = document.getElementById("mylist");
-  var li = document.createElement("li");
-  var nameinput = document.getElementById("nameinput").value;
-  console.log(nameinput);
-  if (nameinput === "") {
-    alert("you need to write something");
-  } else {
-    li.appendChild(document.createTextNode(nameinput));
-    ul.appendChild(li);
-    li.setAttribute("class", "card");
-  }
-  document.getElementById("nameinput").value = "";
-}
+  }).fail(function (err) {
+    console.log(err);
+  });
+  $("#OK").on("click", function () {
+    let title = $("#nameinput").val().trim();
+    let time = $("#date").val() + "  " + $("#time").val();
+    let memo = $("#text1").val();
+    if (!title) return false;
+    $.ajax({
+      url: "http://localhost:3000/todo",
+      method: "post",
+      dataType: "json",
+      data: {
+        title: title,
+        time: time,
+        memo: memo
+      }
+    });
+  });
+
+});
