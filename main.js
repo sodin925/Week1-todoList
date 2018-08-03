afb3dfafd4f5a0e66e92e054fb4ef9a58aeed680
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-08-03 14:03:49 +0800

    OK

--- main.js
+++ main.js
@@ -1,88 +1,7 @@
 /*jshint esversion: 6 */
 
-
-var html = "";
-function createHtml() {
-  html = [
-    '          <div class="card-header" id="item1" name="todo">',
-    '            <input type="checkbox" class="checkbox">',
-    '            <label style="margin-right:400px;">{{title}}</label>',
-    '            <ion-icon class="heart" name="heart-empty"></ion-icon>',
-    '            <ion-icon class="create" name="create" data-toggle="collapse" data-target="#note1" aria-expanded="false" aria-controls="note1"></ion-icon>',
-    '          </div>',
-    '          <div id="note1" class="collapse" aria-labelledby="item1">',
-    '            <div class="card-body">',
-    '              <form>',
-    '                <div>',
-    '                  <label for="text1">',
-    '                    <ion-icon name="build"></ion-icon>名稱</label>',
-    '                  <textarea type="text" class="form-control" id="name1" rows="1">{{title}}</textarea>',
-    '                </div>',
-    '                <div>',
-    '                  <label for="date1">',
-    '                    <ion-icon name="calendar"></ion-icon>到期日</label>',
-    '                  <div class="row">',
-    '                    <div class="col">',
-    '                      <textarea type="text" class="form-control" id="date1" rows="1">{{date}}</textarea>',
-    '                    </div>',
-    '                    <div class="col">',
-    '                      <textarea type="text" class="form-control" id="time1" rows="1">{{time}}</textarea>',
-    '                    </div>',
-    '                  </div>',
-    '                </div>',
-    '                <div>',
-    '                  <label for="file1">',
-    '                    <ion-icon name="attach"></ion-icon>匯入檔案</label>',
-    '                  <input type="file" class="form-control-file" id="file1">',
-    '                </div>',
-    '                <div>',
-    '                  <label for="text1">',
-    '                    <ion-icon name="chatbubbles"></ion-icon>備註</label>',
-    '                  <textarea class="form-control" id="text1" rows="3">{{memo}}</textarea>',
-    '                </div>',
-    '                <div class="form-check">',
-    '                  <input type="checkbox" class="form-check-input" id="exampleCheck1">',
-    '                  <label class="form-check-label" for="exampleCheck1">還沒想好要怎麼用</label>',
-    '                </div>',
-    '              </form>',
-    '            </div>',
-    '          </div>',
-  ].join("");
-  // console.log(html);
-  html.replace("{{title}}", todoTitle);
-  html.replace("{{date}}", todoDate);
-  html.replace("{{time}}", todoTime);
-  html.replace("{{memo}}", todoMemo);
-}
-createHtml();
-
-function checkList() {
-  var allList = mylist.getElementsByTagName("div");
-  var wantToDelete = [];
-  for (var i = 0; i < allList.length; i++) {
-    var thisItem = allList[i];
-    var now = new Date();
-    if (new Date(thisItem.deadLine) < now) {
-      // console.log(thisItem.deadLine);
-      var cloned = document.createElement("div");
-      // console.log(html);
-
-      var newHtml = html;
-      newHtml = newHtml.replace("{{title}}", todoTitle);
-      newHtml = newHtml.replace("{{date}}", todoDate);
-      newHtml = newHtml.replace("{{time}}", todoTime);
-      newHtml = newHtml.replace("{{memo}}", todoMemo);
-
-      thisItem.innerHTML = newHtml;
-      cloned.innerHTML = thisItem.innerHTML;
-      complete.appendChild(cloned);
-      wantToDelete.push(thisItem);
-    }
-  }
-  wantToDelete.forEach(function (a) {
-    a.remove();
-  });
-}
+var complete = [];
+var inProgress = [];
 
 
 var todoTitle = "";
@@ -95,29 +14,33 @@ $(function () {
     method: "get",
     dataType: "json",
     data: {},
-
   }).done(function (res) {
     // console.log(res);
-    $("#mylist").empty();
     res.forEach(function (todo) {
-      var newli = document.createElement("div"); //list
-      var newckb = document.createElement("input"); //checkbox
-      newckb.setAttribute("type", "checkbox");
-      newckb.setAttribute("class", "checkbox");
-
-      todoTitle = todo.title;
-      todoDate = todo.date;
-      todoTime = todo.time;
-      todoMemo = todo.memo;
-
-      newli.deadLine = todo.date + " " + todo.time;
-      newli.innerText = "名稱: " + todo.title + " " + "到期日: " + todo.date + " " + todo.time + " " + "備註: " + todo.memo;
-      newli.appendChild(newckb);
-      document.getElementById("mylist").appendChild(newli);
-
+      var now = new Date();
+      if(new Date(todo.date + " " + todo.time) < now) {
+        complete.push(CreateItem(todo));
+      } else {
+        inProgress.push(CreateItem(todo));
+      }
     });
-    // var scanner = setInterval(checkList, 1000);
-    checkList();
+    complete.forEach(function(zxc) {
+      document.getElementById("complete").appendChild(zxc);
+    });
+    inProgress.forEach(function(asd) {
+      document.getElementById("progress").appendChild(asd);
+    });
+    var scan = setInterval(function (){
+      var now = new Date(); 
+      inProgress.forEach(function(qwe, i) { 
+        if(new Date(qwe.dataset.date + " " + qwe.dataset.time) < now) {
+          document.getElementById("complete").appendChild(qwe);
+          document.getElementById("progress").removeChild(qwe);
+          delete inProgress[i];
+          complete.push(qwe);
+        }
+      });
+    }, 1000);
   }).fail(function (err) {
     console.log(err);
   });
@@ -174,3 +97,59 @@ $("#delete").on("click", function () {
   });
 });
 
+function CreateItem(data) {
+  var div = document.createElement("div");
+  div.innerHTML = [
+    '          <div class="card-header" id="item1" name="todo">',
+    '            <input type="checkbox" class="checkbox">',
+    '            <label style="margin-right:400px;">{{title}}</label>',
+    '            <ion-icon class="heart" name="heart-empty"></ion-icon>',
+    '            <ion-icon class="create" name="create" data-toggle="collapse" data-target="#note1" aria-expanded="false" aria-controls="note1"></ion-icon>',
+    '          </div>',
+    '          <div id="note1" class="collapse" aria-labelledby="item1">',
+    '            <div class="card-body">',
+    '              <form>',
+    '                <div>',
+    '                  <label for="text1">',
+    '                    <ion-icon name="build"></ion-icon>名稱</label>',
+    '                  <textarea type="text" class="form-control" id="name1" rows="1">{{title}}</textarea>',
+    '                </div>',
+    '                <div>',
+    '                  <label for="date1">',
+    '                    <ion-icon name="calendar"></ion-icon>到期日</label>',
+    '                  <div class="row">',
+    '                    <div class="col">',
+    '                      <textarea type="text" class="form-control" id="date1" rows="1">{{date}}</textarea>',
+    '                    </div>',
+    '                    <div class="col">',
+    '                      <textarea type="text" class="form-control" id="time1" rows="1">{{time}}</textarea>',
+    '                    </div>',
+    '                  </div>',
+    '                </div>',
+    '                <div>',
+    '                  <label for="file1">',
+    '                    <ion-icon name="attach"></ion-icon>匯入檔案</label>',
+    '                  <input type="file" class="form-control-file" id="file1">',
+    '                </div>',
+    '                <div>',
+    '                  <label for="text1">',
+    '                    <ion-icon name="chatbubbles"></ion-icon>備註</label>',
+    '                  <textarea class="form-control" id="text1" rows="3">{{memo}}</textarea>',
+    '                </div>',
+    '                <div class="form-check">',
+    '                  <input type="checkbox" class="form-check-input" id="exampleCheck1">',
+    '                  <label class="form-check-label" for="exampleCheck1">還沒想好要怎麼用</label>',
+    '                </div>',
+    '              </form>',
+    '            </div>',
+    '          </div>',
+  ].join("").replace("{{title}}", data.title)
+  .replace("{{date}}", data.date)
+  .replace("{{time}}", data.time)
+  .replace("{{memo}}", data.memo);
+  div.dataset.title = data.title;
+  div.dataset.date = data.date;
+  div.dataset.time = data.time;
+  div.dataset.memo = data.memo;
+  return div;
+}
