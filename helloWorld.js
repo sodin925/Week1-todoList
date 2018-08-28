e9ae871d3f78467bc440ceb9bdcf82251aa95df9
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-08-29 00:51:32 +0800

    1

--- helloWorld.js
+++ helloWorld.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 const server = http.createServer((request, response) => {
-  resquest.statusCode = 200;
-  resquest.setHeader('Content-Type', 'text/plain');
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
   response.end('Hello World');
 });
 server.listen(port, hostname, () => {
