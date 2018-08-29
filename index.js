f60e5acd3b65be2bdb7b58fb4f448c26263a98d5
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-08-30 00:12:43 +0800

    1

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const http = require('http');
-const hostname = '127.0.0.1';
-const PORT = process.env.PORT || 5000;
+
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello World');
-});
-server.listen(PORT, hostname, () => {
-  console.log(`server running at http://${hostname}:${PORT}/`);
-});
+  response.end('Hello World\n');
+}).listen(process.env.PORT);
